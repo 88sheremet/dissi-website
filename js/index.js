68c5ddb97b375cc14a29b7ee1e965f5b9abc3cb4
Oleksandr Sheremet <88sheremet@gmail.com>
2023-05-04 19:31:03 +0300

    add footer and location section

--- js/index.js
+++ js/index.js
@@ -2,24 +2,51 @@ const images = document.querySelectorAll(".slider .slider-line img");
 const sliderLine = document.querySelector(".slider-line");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
-let count = 0;
+let offset = 0;
 
 sliderLine.style.right = "100px";
 
 prevBtn.addEventListener("click", () => {
-  let offset = 0;
+  
   offset = offset - 93;
   if ((offset = 0)) {
     offset = 0;
   }
-  sliderLine.style.right = offset + "0px";
+  sliderLine.style.right = offset + "px";
 });
 
 nextBtn.addEventListener("click", () => {
-  let offset = 0;
+  
   offset = offset + 93;
   if ((offset = 185)) {
     offset = 185;
   }
   sliderLine.style.right = offset + "px";
 });
+
+
+// reviews slider
+
+const sliderLineReviews = document.querySelector(".reviews__slider-list");
+const prevBtnReviews = document.querySelector(".prev-reviews");
+const nextBtnReviews = document.querySelector(".next-reviews");
+
+let change = 0;
+prevBtnReviews.addEventListener("click", () => {
+   
+    change = change - 360;
+    if (change < 0) {
+        change = 0;
+    }
+    sliderLineReviews.style.right = change + "px";
+  });
+  
+  nextBtnReviews.addEventListener("click", () => {
+   
+   
+    change = change + 360;
+    if (change > 720) {
+        change = 720;
+    }
+    sliderLineReviews.style.right = change + "px";
+  });
